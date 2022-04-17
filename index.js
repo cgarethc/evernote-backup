8b494559bf76f892149dab6d833b8077d149b4b2
cgarethc <gareth@cronin.co.nz>
2022-04-18 10:09:13 +1200

    Basic updated logic working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+
 const Evernote = require('evernote');
 const config = require('./config');
-
+const sanitise = require("./sanitise").sanitise;
+const sanitize = require("sanitize-filename");
 
 const client = new Evernote.Client({
   sandbox: true, // change to false when you are ready to switch to production
@@ -10,26 +13,76 @@ const client = new Evernote.Client({
 
 (async () => {
 
+  if (!fs.existsSync('./files')) {
+    fs.mkdirSync('./files');
+  }
+
   var noteStore = client.getNoteStore();
 
   const notebooks = await noteStore.listNotebooks();
-  for (let notebook of notebooks) {
-    const count = await noteStore.findNoteCounts({ notebookGuid: notebook.guid }, false);
-    const totalNotes = count.notebookCounts[notebook.guid];
-    console.log(`\n*** ${notebook.name} ${totalNotes} ***`);
-    const spec = { includeTitle: true, includeUpdated: true, includeContentLength: true };
-    const metadata = await noteStore.findNotesMetadata({ notebookGuid: notebook.guid }, 0, 250, spec);
+  let lastProcessedDate;
 
-    for (let notemeta of metadata.notes) {
-      console.log(`${notemeta.title} - ${notemeta.contentLength}B - ${new Date(notemeta.updated)} ${notemeta.guid}`);
-      const note = await noteStore.getNote(notemeta.guid, true, false, false, false);
-      if (note.resources) {
-        console.log(`Note has ${note.resources.length} resources`);
-      }
-      console.log(note.content);
+  for (let notebook of notebooks) {
+
+    const sanitizedFileName = `./files/${sanitise(sanitize(notebook.name))}`;
+    const infoFileName = `${sanitizedFileName}/info.json`;
+    console.debug('Checking if notebook exists on disk already', sanitizedFileName);
+    if (!fs.existsSync(sanitizedFileName)) {
+      // never seen this notebook before, create the directory for it
+      console.debug(`Directory doesn't exist, creating it`);
+      processThisNotebook = true;
+      fs.mkdirSync(sanitizedFileName);
+    }
+    else if (fs.existsSync(infoFileName)) {
+      const info = JSON.parse(fs.readFileSync(infoFileName, 'utf8'));
+      console.debug('last processed', info.processed);
+      lastProcessedDate = new Date(info.processed);
     }
 
+    const count = await noteStore.findNoteCounts({ notebookGuid: notebook.guid }, false);
+    const totalNotes = count.notebookCounts[notebook.guid];
+    console.debug(`\n*** ${notebook.name} ${totalNotes} ***`);
+
+    // page through using the maximum fetch of 250
+
+    for (let counter = 0; counter < totalNotes; counter += 250) {
+
+      console.debug('calling with offset', counter);
+
+      const spec = { includeTitle: true, includeUpdated: true, includeContentLength: true };
+      const metadata = await noteStore.findNotesMetadata({ notebookGuid: notebook.guid, order: 2, ascending: false }, counter, 250, spec);
+
+      // get the metadata
+      for (let noteMeta of metadata.notes) {
+        console.debug(`\n "${noteMeta.title}" (${noteMeta.contentLength}B) updated ${new Date(noteMeta.updated)} ${noteMeta.guid}`);
+
+        // if this is the first run for the notebook 
+        // or the note has been updated since the last time we processed the notebook, process it
+        if (!lastProcessedDate || noteMeta.updated > lastProcessedDate) {
+          const note = await noteStore.getNote(noteMeta.guid, true, false, false, false);
+          if (note.resources) {
+            console.debug(`Note has ${note.resources.length} resources`);
+          }
+          console.debug(note.content);
+        }
+
+        // if this is not the first run for the notebook and the note we have reached was updated 
+        // earlier than the last time we processed the notebook, we can stop processing the notebook
+        if (lastProcessedDate && noteMeta.updated <= lastProcessedDate) {
+          console.debug('Note updated earlier than last processed date, stopping');
+          break;
+        }
+
+      }
+    }
+
+    // update the last processed time for the notebook
+    console.debug('writing info.json');
+    fs.writeFileSync(infoFileName, JSON.stringify({ processed: new Date().toISOString() }));
+
+
   }
+
 })();
 
 
---
+++ sanitise.js
@@ -0,0 +1,64 @@
+const emojiStrip = require('emoji-strip');
+const accents = require('remove-accents');
+
+const macronMap = {
+  'ā': 'aa',
+  'ē': 'ee',
+  'ī': 'ii',
+  'ō': 'oo',
+  'ū': 'uu',
+  'Ā': 'AA',
+  'Ē': 'EE',
+  'Ī': 'II',
+  'Ō': 'OO',
+  'Ū': 'UU'
+};
+
+/*
+< (less than)
+> (greater than)
+: (colon)
+" (double quote)
+/ (forward slash)
+\ (backslash)
+| (vertical bar or pipe)
+? (question mark)
+* (asterisk)
+*/
+const OEMreservedMap = {
+  '<': '[lt]',
+  '>': '[gt]',
+  ':': ' ',
+  '"': `'`,
+  '/': ' ',
+  '\\': ' ',
+  '|': ' ',
+  '?': ' ',
+  '*': ' '
+};
+
+exports.sanitiseMacrons = (text) => {
+  return text.replace(/[āēīōūĀĒĪŌŪ]/g, (match) => {
+    return macronMap[match];
+  });
+}
+
+exports.sanitiseOEMReserved = (text) => {
+  return text.replace(/[<>:"/\\|?*]/g, (match) => {
+    return OEMreservedMap[match];
+  });
+}
+
+exports.sanitiseSmartQuotes = (text) => {
+  return text.replace(/[‘’]/g, "'").replace(/[“”]/g, '"');
+};
+
+exports.sanitise = (text) => {
+  let sanitised = text;
+  sanitised = exports.sanitiseMacrons(text);
+  sanitised = emojiStrip(sanitised);
+  sanitised = accents.remove(sanitised);
+  sanitised = exports.sanitiseSmartQuotes(sanitised);
+  sanitised = exports.sanitiseOEMReserved(sanitised);
+  return sanitised;
+};
